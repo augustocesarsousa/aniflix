756cdc85b058624502bfa6f1cc47e9b1f70a11fb
Augusto Cesar <acs-03@hotmail.com>
2022-04-19 22:23:15 -0300

    Recomended carousel

---
+++ js/owl/main.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:20,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        992:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
